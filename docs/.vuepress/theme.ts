9375a15a8d7a7ecdb81e227a78a3bd6404a59282
topjfy <topjfk@163.com>
2025-04-30 10:47:53 +0800

    new 更新主题版本rc.83

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -1,4 +1,4 @@
-import { getDirname, path } from "@vuepress/utils";
+import { getDirname, path } from "vuepress/utils";
 import { hopeTheme } from "vuepress-theme-hope";
 import { allNavbar } from "./navbar.ts";
 import { allSidebar } from "./sidebar/index";
@@ -33,8 +33,8 @@ export default hopeTheme({
 
     // 关键词: "iconfont", "iconify", "fontawesome", "fontawesome-with-brands"
     // 参考：https://theme-hope.vuejs.press/zh/guide/interface/icon.html#设置图标资源
-    iconAssets: "iconfont",
-    iconPrefix: "iconfont icon-",
+    //iconAssets: "iconfont",
+    //iconPrefix: "iconfont icon-",
 
     pageInfo: ["Author", "Category", "Tag", "Date", "Original", "Word", "PageView", "ReadingTime"],
 
@@ -75,22 +75,63 @@ export default hopeTheme({
     //footer: "", //'<a href="https://beian.miit.gov.cn/" target="_blank">黔ICP备2022xxxxxx号-1</a>',
 
     displayFooter: true,
+    
+    //md增强
+    markdown: {
+        align: true,
+        attrs: true,
+        chartjs: true,
+        //代码块分组
+        codeTabs: true,
+        demo: true,
+        echarts: true,
+        gfm: true,
+        include: true,
+        math: {
+            type: "katex", // 或 'mathjax'
+        },
+        mark: true,
+        mermaid: true,
+        //交互演示
+        playground: {
+            presets: ["ts", "vue"],
+        },
+        //幻灯片
+        revealjs: true,
+        sub: true,
+        sup: true,
+        tabs: true,
+        vPre: true,
+        vuePlayground: true,
+        //流程图
+        flowchart: true,
+        //脚注
+        footnote: true,
+        //自定义容器:提示、注释、信息、注意、警告和详情
+        hint: true,
+        // 启用图片大小
+        imgLazyload: true,
+        // 启用图片标记
+        imgMark: true,
+        imgSize: true,
+        figure: true,
+    },
 
     plugins: {
         //搜索插件
-        searchPro: {
-            indexContent: true,
-            customFields: [
-                {
-                    getter: (page) => page.frontmatter.category,
-                    formatter: "分类：$content",
-                },
-                {
-                    getter: (page) => page.frontmatter.tag,
-                    formatter: "标签：$content",
-                },
-            ],
-        },
+        // searchPro: {
+        //     indexContent: true,
+        //     customFields: [
+        //         {
+        //             getter: (page) => page.frontmatter.category,
+        //             formatter: "分类：$content",
+        //         },
+        //         {
+        //             getter: (page) => page.frontmatter.tag,
+        //             formatter: "标签：$content",
+        //         },
+        //     ],
+        // },
 
         //评论模块
         // comment: {
@@ -114,51 +155,13 @@ export default hopeTheme({
                 "VPBanner", //用于展示 banner
                 "VPCard", //卡片组件，可用于展示项目。
                 "VidStack", //视频播放器
-                "XiGua", //西瓜视频
+                // "XiGua", //西瓜视频
             ],
             componentOptions: {
                 pdf: {
                     pdfjs: "/assets/lib/pdfjs",
                 },
             },
-        },
-
-        //md增强
-        mdEnhance: {
-            align: true,
-            attrs: true,
-            chart: true,
-            codetabs: true,
-            demo: true,
-            echarts: true,
-            gfm: true,
-            include: true,
-            katex: true,
-            mark: true,
-            mermaid: true,
-            //交互演示
-            playground: {
-                presets: ["ts", "vue"],
-            },
-            //幻灯片
-            revealJs: true,
-            sub: true,
-            sup: true,
-            tabs: true,
-            vPre: true,
-            vuePlayground: true,
-            //流程图
-            flowchart: true,
-            //脚注
-            footnote: true,
-            //自定义容器:提示、注释、信息、注意、警告和详情
-            hint: true,
-            // 启用图片大小
-            imgLazyload: true,
-            // 启用图片标记
-            imgMark: true,
-            imgSize: true,
-            figure: true,
         },
 
         photoSwipe: true,
@@ -168,10 +171,8 @@ export default hopeTheme({
         },
 
         pwa: {
-            update: "hint",
-            cachePic: true,
+            update: "available", // available、hint、disabled、force
             maxSize: 1024 * 8,
-            maxPicSize: 1024 * 6,
             //是否缓存除主页和 404 错误页之外的 HTML 文件
             cacheHTML: false,
             appendBase: true,
